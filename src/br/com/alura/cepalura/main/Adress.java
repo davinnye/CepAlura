28d67cb11699ef4cb5a4a54eeadd2532ac000087
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2024-08-19 19:13:47 -0300

    course corrections - stage 1

---
+++ src/br/com/alura/cepalura/main/Adress.java
@@ -0,0 +1,7 @@
+package br.com.alura.cepalura.main;
+
+public record Adress(String cep, String logradouro,
+        String bairro, String localidade,
+        String uf, String ibge, String gia, double ddd) {
+
+}
---
+++ src/br/com/alura/cepalura/main/Searcher.java
@@ -0,0 +1,32 @@
+package br.com.alura.cepalura.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.google.gson.Gson;
+
+public class Searcher {
+
+    public Adress searchAdress(String cep) {
+
+        URI url = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(url)
+                .build();
+        HttpResponse<String> response;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Adress.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não consegui obter a partir desse cep");
+        }
+
+    }
+
+}
